62283474aa9f2ffd74a81330384f5ab75e80cecd
Elmar Ramazanov <22112363@student.ciu.edu.tr>
2026-07-01 21:53:12 +0300

    feat: add HeadBlock, Hero, SuperBox components and enhance Navbar with dropdown functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import SuperBox from './components/SuperBox'
+import HeadBlock from './components/HeadBlock'
 
 function App() {
   return (
     <>
       <Header />
       <Navbar />
+      <Hero />
+      <SuperBox />
+      <HeadBlock />
+      
     </>
   )
 }
